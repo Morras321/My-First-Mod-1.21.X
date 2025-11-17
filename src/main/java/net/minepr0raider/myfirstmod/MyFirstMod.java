fffefeb029644e0d61b30db21f40b947f29e81ea
Morras321 <rascustom123@gmail.com>
2025-11-17 14:54:32 +0200

    Initial commit

---
+++ src/main/java/net/minepr0raider/myfirstmod/MyFirstMod.java
@@ -0,0 +1,16 @@
+package net.minepr0raider.myfirstmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyFirstMod implements ModInitializer {
+	public static final String MOD_ID = "myfirstmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/minepr0raider/myfirstmod/MyFirstModClient.java
@@ -0,0 +1,10 @@
+package net.minepr0raider.myfirstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyFirstModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
